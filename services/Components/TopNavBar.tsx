18b3f23cdbe0895b94e3a1c2653764da2e31f358
Pratyush2004-oss <pratyush@sitp.ac.in>
2025-06-25 22:02:45 +0530

    adding shape functionality

--- services/Components/TopNavBar.tsx
+++ services/Components/TopNavBar.tsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import ShapesSetting from '../Sharable/ShapesSetting'
+import React, { useEffect, useState } from "react";
+import ShapesSetting from "../Sharable/ShapesSetting";
+import { useCanvasHook } from "@/app/(routes)/design/[designId]/page";
 
 function TopNavBar() {
+  const { canvasEditor } = useCanvasHook();
+  const [showShapeSetting, setshowShapeSetting] = useState(false)
+
+  useEffect(() => {
+    const activeObject = canvasEditor?.getActiveObject();
+
+  }, [canvasEditor]);
+  if(canvasEditor) {
+    canvasEditor.on('selection:created', function(e: any) {
+      const activeObject = canvasEditor.getActiveObject();
+      if (activeObject && e.selected[0].cornerStyle == 'rect') {
+        setshowShapeSetting(true);
+      }
+    })
+    canvasEditor.on('selection:cleared', function() {
+      setshowShapeSetting(false);
+    });
+  }
   return (
-    <div className='p-3 bg-white'>
-        <ShapesSetting/>
+    <div className="p-3 bg-white">
+      {showShapeSetting && <ShapesSetting />}
     </div>
-  )
+  );
 }
 
-export default TopNavBar
+export default TopNavBar;
